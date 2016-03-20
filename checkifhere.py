d0cd348d1b6ad9e4fcc55ef0ac7ef2df2514d3ce
kewalkishang <kewalkishang@gmail.com>
2016-03-20 21:35:01 +0530

    Second commit

---
+++ checkifhere.py
@@ -0,0 +1,19 @@
+print("hello")
+label: labelaa
+va=input("input - ")
+varr=0;
+word="kewalkk"
+okay="_ew_l__"
+hi=list(okay)
+if(va in word):
+   for le in word:
+     if(le==va): 
+        okay=okay.replace(okay[varr],va)
+        hi[varr]=va
+     varr=varr+1
+
+okay=' '.join(hi)     
+print(okay)
+print(hi)
+goto labelaa
+
---
+++ goto.py
@@ -0,0 +1,7 @@
+print("starting")
+z=" "
+for i in range(6):
+    z+=" "+input("tell me - ")
+
+print("end")
+print(z)
